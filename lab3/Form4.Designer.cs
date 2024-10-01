01cbbe0f395422d5f169aa9ea1a4e42d17cef5c5
Ivan Ovcharov <xxovcharovxx@gmail.com>
2024-10-02 00:14:09 +0300

    feat: task 3

--- lab3/Form4.Designer.cs
+++ lab3/Form4.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.palettePic = new System.Windows.Forms.PictureBox();
+            this.colorLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.palettePic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(425, 65);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(793, 474);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            this.pic.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pic_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(33, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(358, 93);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Стереть все";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Erase_Click);
+            // 
+            // palettePic
+            // 
+            this.palettePic.Image = ((System.Drawing.Image)(resources.GetObject("palettePic.Image")));
+            this.palettePic.Location = new System.Drawing.Point(33, 180);
+            this.palettePic.Name = "palettePic";
+            this.palettePic.Size = new System.Drawing.Size(358, 359);
+            this.palettePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.palettePic.TabIndex = 3;
+            this.palettePic.TabStop = false;
+            this.palettePic.MouseClick += new System.Windows.Forms.MouseEventHandler(this.palettePic_MouseClick);
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.AutoSize = true;
+            this.colorLabel.BackColor = System.Drawing.Color.White;
+            this.colorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.colorLabel.Location = new System.Drawing.Point(645, 582);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(307, 52);
+            this.colorLabel.TabIndex = 4;
+            this.colorLabel.Text = "Текущий цвет";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1249, 687);
+            this.Controls.Add(this.colorLabel);
+            this.Controls.Add(this.palettePic);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pic);
+            this.Name = "Form4";
             this.Text = "Form4";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.palettePic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox palettePic;
+        private System.Windows.Forms.Label colorLabel;
     }
 }
--- lab3/Form4.cs
+++ lab3/Form4.cs
@@ -12,9 +12,222 @@ namespace lab3
 {
     public partial class Form4 : Form
     {
+        Graphics _graphics;
+        Bitmap bitmap;
+        Point[] p = new Point[3];
+        Color[] colors = new Color[3];
+        int index = 0;
+        Pen pen = new Pen(Color.Red, 1);
+        Bitmap paletteBitmap;
+
         public Form4()
         {
             InitializeComponent();
+            bitmap = new Bitmap(pic.Width, pic.Height);
+            _graphics = Graphics.FromImage(bitmap);
+            _graphics.Clear(Color.White);
+            pic.Image = bitmap;
+            paletteBitmap = new Bitmap(palettePic.Image);
+        }
+
+        private void pic_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (index < 2)
+            {
+                p[index] = e.Location;
+                colors[index] = pen.Color;
+                index++;
+                _graphics.DrawRectangle(pen, e.Location.X, e.Location.Y, 1, 1);
+                pic.Refresh();
+            }
+            else
+            {
+                colors[2] = pen.Color;
+                p[2] = e.Location;
+                _graphics.DrawRectangle(pen, e.Location.X, e.Location.Y, 1, 1);
+                DrawTriangle();
+                pic.Refresh();
+                pen.Color = colorLabel.BackColor;
+                index = 0;
+            }
+        }
+
+        private void palettePic_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (paletteBitmap != null)
+            {
+                int paletteWidth = palettePic.Image.Width;
+                int paletteHeight = palettePic.Image.Height;
+                if (palettePic.SizeMode == PictureBoxSizeMode.StretchImage || palettePic.SizeMode == PictureBoxSizeMode.Zoom)
+                {
+                    float scaleX = (float)paletteWidth / palettePic.Width;
+                    float scaleY = (float)paletteHeight / palettePic.Height;
+                    int realX = (int)(e.X * scaleX);
+                    int realY = (int)(e.Y * scaleY);
+
+                    if (realX >= 0 && realX < paletteBitmap.Width && realY >= 0 && realY < paletteBitmap.Height)
+                    {
+                        Color selectedColor = paletteBitmap.GetPixel(realX, realY);
+                        pen.Color = selectedColor;
+                        colorLabel.BackColor = selectedColor;
+                    }
+                }
+                else
+                {
+                    if (e.X >= 0 && e.X < paletteBitmap.Width && e.Y >= 0 && e.Y < paletteBitmap.Height)
+                    {
+                        Color selectedColor = paletteBitmap.GetPixel(e.X, e.Y);
+                        pen.Color = selectedColor;
+                        colorLabel.BackColor = selectedColor;
+                    }
+                }
+            }
+        }
+
+        private void DrawTriangle()
+        {
+            if (p[1].Y < p[0].Y) Swap(0, 1);
+            if (p[2].Y < p[0].Y) Swap(0, 2);
+            if (p[2].Y < p[1].Y) Swap(1, 2);
+
+            int top_y = p[0].Y;
+
+            while (top_y < p[1].Y)
+            {
+                float Xleft, Xright;
+                Xleft = InterpolateX(p[0], p[1], p[2], top_y, true, false);
+                Xright = InterpolateX(p[0], p[1], p[2], top_y, false, false);
+                Color Cleft = InterpolateColor(p[0], p[1], colors[0], colors[1], top_y);
+                Color Cright = InterpolateColor(p[0], p[2], colors[0], colors[2], top_y);
+
+                if (Xleft > Xright)
+                {
+                    (Xleft, Xright) = (Xright, Xleft);
+                    (Cleft, Cright) = (Cright, Cleft);
+                }
+
+                for (float x = Xleft; x <= Xright; x++)
+                {
+                    float t = (x - Xleft) / (Xright - Xleft);
+                    Color interpolatedColor = InterpolateColor(Cleft, Cright, t);
+                    pen.Color = interpolatedColor;
+                    _graphics.DrawRectangle(pen, x, top_y, 1, 1);
+                }
+                top_y++;
+            }
+
+            while (top_y < p[2].Y)
+            {
+                float Xleft, Xright;
+                Xleft = InterpolateX(p[0], p[1], p[2], top_y, true, true);
+                Xright = InterpolateX(p[0], p[1], p[2], top_y, false, true);
+                Color Cleft = InterpolateColor(p[1], p[2], colors[1], colors[2], top_y);
+                Color Cright = InterpolateColor(p[0], p[2], colors[0], colors[2], top_y);
+
+                if (Xleft > Xright)
+                {
+                    (Xleft, Xright) = (Xright, Xleft);
+                    (Cleft, Cright) = (Cright, Cleft);
+                }
+
+                for (float x = Xleft; x <= Xright; x++)
+                {
+                    float t = (x - Xleft) / (Xright - Xleft);
+                    Color interpolatedColor = InterpolateColor(Cleft, Cright, t);
+                    pen.Color = interpolatedColor;
+                    _graphics.DrawRectangle(pen, x, top_y, 1, 1);
+                }
+                top_y++;
+            }
+        }
+
+
+        private float InterpolateX(Point p0, Point p1, Point p2, float y, bool isLeft, bool afterMid)
+        {
+            if (!afterMid)
+            {
+                if (isLeft)
+                {
+                    if (p0.Y == p1.Y) 
+                        return p0.X;
+
+                    return p0.X + (p1.X - p0.X) * (y - p0.Y) / (p1.Y - p0.Y);
+                }
+                else
+                {
+                    if (p0.Y == p2.Y) 
+                        return p0.X;
+
+                    return p0.X + (p2.X - p0.X) * (y - p0.Y) / (p2.Y - p0.Y);
+                }
+            }
+            else
+            {
+                if (isLeft)
+                {
+                    if (p1.Y == p2.Y) 
+                        return p1.X;
+
+                    return p1.X + (p2.X - p1.X) * (y - p1.Y) / (p2.Y - p1.Y);
+                }
+                else
+                {
+                    if (p0.Y == p2.Y) 
+                        return p0.X;
+
+                    return p0.X + (p2.X - p0.X) * (y - p0.Y) / (p2.Y - p0.Y);
+                }
+            }
+        }
+
+        private Color InterpolateColor(Point p0, Point p1, Color c0, Color c1, float y)
+        {
+            if (p0.Y == p1.Y) 
+                return c0;
+
+            float t = (y - p0.Y) / (p1.Y - p0.Y);
+            return InterpolateColor(c0, c1, t);
+        }
+
+        private Color InterpolateColor(Color c1, Color c2, float t)
+        {
+            int r = (int)Math.Round(c1.R + (c2.R - c1.R) * t);
+            int g = (int)Math.Round(c1.G + (c2.G - c1.G) * t);
+            int b = (int)Math.Round(c1.B + (c2.B - c1.B) * t);
+
+            r = Clamp(r, 0, 255);
+            g = Clamp(g, 0, 255);
+            b = Clamp(b, 0, 255);
+
+            return Color.FromArgb(r, g, b);
+        }
+
+        private int Clamp(int value, int min, int max)
+        {
+            if (value < min) 
+                return min;
+            if (value > max) 
+                return max;
+
+            return value;
+        }
+
+        private void Swap(int i, int j)
+        {
+            Point tempPoint = p[i];
+            p[i] = p[j];
+            p[j] = tempPoint;
+
+            Color tempColor = colors[i];
+            colors[i] = colors[j];
+            colors[j] = tempColor;
+        }
+
+        private void Erase_Click(object sender, EventArgs e)
+        {
+            _graphics.Clear(Color.White);
+            index = 0;
+            pic.Refresh();
         }
     }
 }
