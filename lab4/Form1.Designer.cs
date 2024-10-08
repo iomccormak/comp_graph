43090302b479dc7d7e51ea77f624a4a1cfef72d4
Ivan Ovcharov <xxovcharovxx@gmail.com>
2024-10-08 23:51:36 +0300

    feat: task4

--- lab4/Form1.Designer.cs
+++ lab4/Form1.Designer.cs
@@ -31,14 +31,20 @@
             this.deleteButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.polygonSelectDropDown = new System.Windows.Forms.ComboBox();
+            this.applyButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // deleteButton
             // 
-            this.deleteButton.Location = new System.Drawing.Point(12, 500);
+            this.deleteButton.Location = new System.Drawing.Point(16, 615);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(4);
             this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(130, 31);
+            this.deleteButton.Size = new System.Drawing.Size(173, 38);
             this.deleteButton.TabIndex = 0;
             this.deleteButton.Text = "Очистить";
             this.deleteButton.UseVisualStyleBackColor = true;
@@ -46,9 +52,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(148, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(197, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(704, 519);
+            this.pictureBox1.Size = new System.Drawing.Size(939, 639);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
@@ -57,22 +64,78 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 15);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(130, 17);
+            this.radioButton1.Size = new System.Drawing.Size(164, 20);
             this.radioButton1.TabIndex = 3;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Нарисовать полигон";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(16, 394);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(173, 214);
+            this.outputTextBox.TabIndex = 4;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(157, 20);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Точка пересечения";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(16, 68);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(140, 20);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Принадлежность";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // polygonSelectDropDown
+            // 
+            this.polygonSelectDropDown.FormattingEnabled = true;
+            this.polygonSelectDropDown.Location = new System.Drawing.Point(16, 356);
+            this.polygonSelectDropDown.Name = "polygonSelectDropDown";
+            this.polygonSelectDropDown.Size = new System.Drawing.Size(173, 24);
+            this.polygonSelectDropDown.TabIndex = 7;
+            // 
+            // applyButton
+            // 
+            this.applyButton.Location = new System.Drawing.Point(13, 95);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(167, 50);
+            this.applyButton.TabIndex = 8;
+            this.applyButton.Text = "Apply";
+            this.applyButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(864, 543);
+            this.ClientSize = new System.Drawing.Size(1152, 668);
+            this.Controls.Add(this.applyButton);
+            this.Controls.Add(this.polygonSelectDropDown);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.outputTextBox);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.deleteButton);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -86,6 +149,11 @@
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ComboBox polygonSelectDropDown;
+        private System.Windows.Forms.Button applyButton;
     }
 }
 
--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -17,7 +17,11 @@ namespace lab4
         private Bitmap _bitmap;
         private Graphics _graphics;
         private Pen _pen;
-
+        private int intFlag = 0;
+        private List<Point> tempEdge = new List<Point>();
+        private Color polygonColor = Color.Black;
+        private Color selectedPolygonColor = Color.Red;
+        Point tempPoint; 
         public Form1()
         {
             InitializeComponent();
@@ -25,11 +29,22 @@ namespace lab4
             _graphics = Graphics.FromImage(_bitmap);
             _graphics.Clear(Color.White);
             _pen = new Pen(Color.Black, 2);
+            tempPoint = new Point(); // Потому что больше негде
             pictureBox1.Image = _bitmap;
 
-            //pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
-            //pictureBox1.Paint += new PaintEventHandler(pictureBox1_Paint);
-        } 
+            // Инициализация радио-кнопки для пересечения
+            radioButton2.CheckedChanged += RadioButtonIntersection_CheckedChanged;
+        }
+
+        private void RadioButtonIntersection_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                intFlag = 0;
+                tempEdge.Clear();
+                outputTextBox.Text = "Выберите 4 точки для проверки пересечения.";
+            }
+        }
 
         private void ClearButton_Click(object sender, EventArgs e)
         {
@@ -46,19 +61,58 @@ namespace lab4
                 if (e.Button == MouseButtons.Left)
                 {
                     DrawPolygon(e);
-                } 
+                }
                 else if (e.Button == MouseButtons.Right)
                 {
                     StopDrawPolygon();
                 }
             }
+            else if (radioButton2.Checked)
+            {
+                HandleIntersectionMode(e);
+            }
+            else if (radioButton3.Checked)
+            {
+               
+            }
         }
+
+        private void HandleIntersectionMode(MouseEventArgs e)
+        {
+            if (intFlag < 4)
+            {
+                tempEdge.Add(e.Location);
+                _bitmap.SetPixel(e.Location.X, e.Location.Y, polygonColor);
+                intFlag++;
+
+                if (tempEdge.Count % 2 == 0)
+                {
+                    _graphics.DrawLine(new Pen(polygonColor), tempEdge[tempEdge.Count - 2], tempEdge[tempEdge.Count - 1]);
+                }
+                pictureBox1.Image = _bitmap;
+
+                if (intFlag == 4)
+                {
+                    CheckIntersection();
+                }
+            }
+            else if (intFlag == 5)
+            {
+                _graphics.Clear(Color.White);
+                pictureBox1.Image = _bitmap;
+
+                tempEdge.Clear();
+                intFlag = 0;
+                outputTextBox.Text = "Выберите 4 точки для нового пересечения.";
+            }
+        }
+
 
         private void StopDrawPolygon()
         {
             if (currentPolygon.Count > 0)
             {
-                polygons.Add(new List<Point>(currentPolygon)); 
+                polygons.Add(new List<Point>(currentPolygon));
                 currentPolygon.Clear();
                 pictureBox1.Invalidate();
             }
@@ -72,42 +126,91 @@ namespace lab4
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            // Рисуем все полигоны
             foreach (var polygon in polygons)
             {
                 if (polygon.Count == 1)
                 {
-                    e.Graphics.DrawEllipse(_pen, polygon[0].X, polygon[0].Y, 2, 2); // Рисуем точку
+                    e.Graphics.DrawEllipse(_pen, polygon[0].X, polygon[0].Y, 2, 2);
                 }
                 else if (polygon.Count >= 2)
                 {
                     for (int i = 0; i < polygon.Count - 1; i++)
                     {
-                        e.Graphics.DrawLine(_pen, polygon[i], polygon[i + 1]); // Рисуем ребро
+                        e.Graphics.DrawLine(_pen, polygon[i], polygon[i + 1]);
                     }
                     if (polygon.Count > 2)
                     {
-                        e.Graphics.DrawPolygon(_pen, polygon.ToArray()); // Рисуем полигон
+                        e.Graphics.DrawPolygon(_pen, polygon.ToArray());
                     }
                 }
             }
 
-            // Рисуем текущий полигон
             if (currentPolygon.Count == 1)
             {
-                e.Graphics.DrawEllipse(_pen, currentPolygon[0].X, currentPolygon[0].Y, 2, 2); // Рисуем точку
+                e.Graphics.DrawEllipse(_pen, currentPolygon[0].X, currentPolygon[0].Y, 2, 2);
             }
             else if (currentPolygon.Count >= 2)
             {
                 for (int i = 0; i < currentPolygon.Count - 1; i++)
                 {
-                    e.Graphics.DrawLine(_pen, currentPolygon[i], currentPolygon[i + 1]); // Рисуем ребро
+                    e.Graphics.DrawLine(_pen, currentPolygon[i], currentPolygon[i + 1]);
                 }
                 if (currentPolygon.Count > 2)
                 {
-                    e.Graphics.DrawPolygon(_pen, currentPolygon.ToArray()); // Рисуем полигон
+                    e.Graphics.DrawPolygon(_pen, currentPolygon.ToArray());
                 }
             }
         }
+
+        private Point? FindIntersection(Point a, Point b, Point c, Point d, out string message)
+        {
+            Point n = new Point(-(d.Y - c.Y), d.X - c.X);
+
+            float denominator = n.X * (b.X - a.X) + n.Y * (b.Y - a.Y);
+            if (denominator == 0)
+            {
+                message = "Прямые параллельны.";
+                return null;
+            }
+
+            float t = -(float)(n.X * (a.X - c.X) + n.Y * (a.Y - c.Y)) / denominator;
+
+            Point res = new Point(a.X + (int)(t * (b.X - a.X)), a.Y + (int)(t * (b.Y - a.Y)));
+
+            if (res.X >= Math.Min(a.X, b.X) && res.X <= Math.Max(a.X, b.X) &&
+                res.Y >= Math.Min(a.Y, b.Y) && res.Y <= Math.Max(a.Y, b.Y) &&
+                res.X >= Math.Min(c.X, d.X) && res.X <= Math.Max(c.X, d.X) &&
+                res.Y >= Math.Min(c.Y, d.Y) && res.Y <= Math.Max(c.Y, d.Y))
+            {
+                message = $"Точка пересечения - ({res.X}, {res.Y}).";
+                return res;
+            }
+            else
+            {
+                message = "Рёбра не пересекаются.";
+                return null;
+            }
+        }
+
+        private void CheckIntersection()
+        {
+            Point a = tempEdge[0];
+            Point b = tempEdge[1];
+            Point c = tempEdge[2];
+            Point d = tempEdge[3];
+
+            string message;
+            Point? intersection = FindIntersection(a, b, c, d, out message);
+
+            if (intersection.HasValue)
+            {
+                _graphics.DrawRectangle(new Pen(selectedPolygonColor), intersection.Value.X, intersection.Value.Y, 1, 1);
+                pictureBox1.Image = _bitmap;
+            }
+
+            outputTextBox.Text = message + " Нажмите на экран, чтобы продолжить.";
+            intFlag = 5; // Переход в следующий режим
+        }
     }
+
 }
