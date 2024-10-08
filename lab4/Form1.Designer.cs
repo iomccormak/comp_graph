cbb6e606933e40e8258649384ac634bb40e4616a
Stuzja <ann.shishkina04@mail.ru>
2024-10-09 01:36:52 +0300

    added athenian transformations

--- lab4/Form1.Designer.cs
+++ lab4/Form1.Designer.cs
@@ -31,6 +31,15 @@
             this.deleteButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.comboBoxAthenian = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxPolygon = new System.Windows.Forms.ComboBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,7 +66,7 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Location = new System.Drawing.Point(4, 12);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(130, 17);
             this.radioButton1.TabIndex = 3;
@@ -65,11 +74,106 @@
             this.radioButton1.Text = "Нарисовать полигон";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
+            // buttonApply
+            // 
+            this.buttonApply.Location = new System.Drawing.Point(12, 462);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(129, 32);
+            this.buttonApply.TabIndex = 4;
+            this.buttonApply.Text = "Принять";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // comboBoxAthenian
+            // 
+            this.comboBoxAthenian.FormattingEnabled = true;
+            this.comboBoxAthenian.Items.AddRange(new object[] {
+            "Не выбрано",
+            "Смещение на dx,dy",
+            "Поворот вокруг заданной точки",
+            "Поворот вокруг центра",
+            "Масштабирование относительно заданной точки",
+            "Масштабирование относительно центра"});
+            this.comboBoxAthenian.Location = new System.Drawing.Point(4, 48);
+            this.comboBoxAthenian.Name = "comboBoxAthenian";
+            this.comboBoxAthenian.Size = new System.Drawing.Size(137, 21);
+            this.comboBoxAthenian.TabIndex = 5;
+            this.comboBoxAthenian.SelectedIndexChanged += new System.EventHandler(this.comboBoxAthenian_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Афинные преобразования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Выбранный полигон";
+            // 
+            // comboBoxPolygon
+            // 
+            this.comboBoxPolygon.FormattingEnabled = true;
+            this.comboBoxPolygon.Items.AddRange(new object[] {
+            "Не выбран"});
+            this.comboBoxPolygon.Location = new System.Drawing.Point(4, 88);
+            this.comboBoxPolygon.Name = "comboBoxPolygon";
+            this.comboBoxPolygon.Size = new System.Drawing.Size(136, 21);
+            this.comboBoxPolygon.TabIndex = 8;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(4, 128);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(136, 20);
+            this.textBoxInput.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ввод";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Вывод";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(5, 168);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(135, 20);
+            this.textBoxOutput.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(864, 543);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.comboBoxPolygon);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxAthenian);
+            this.Controls.Add(this.buttonApply);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.deleteButton);
@@ -86,6 +190,15 @@
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.ComboBox comboBoxAthenian;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxPolygon;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxOutput;
     }
 }
 
--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -10,6 +10,16 @@ using System.Windows.Forms;
 
 namespace lab4
 {
+    enum Mode
+    {
+        None,
+        Move_Polygon,
+        Turn_Around_Point,
+        Turn_Around_Center,
+        Scaling_Relative_To_Point,
+        Scaling_Relative_To_Center
+    }
+
     public partial class Form1 : Form
     {
         private List<List<Point>> polygons = new List<List<Point>>();
@@ -17,6 +27,7 @@ namespace lab4
         private Bitmap _bitmap;
         private Graphics _graphics;
         private Pen _pen;
+        private Mode _mode;
 
         public Form1()
         {
@@ -26,7 +37,8 @@ namespace lab4
             _graphics.Clear(Color.White);
             _pen = new Pen(Color.Black, 2);
             pictureBox1.Image = _bitmap;
-
+            comboBoxAthenian.SelectedIndex = 0;
+            comboBoxPolygon.SelectedIndex = 0;
             //pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
             //pictureBox1.Paint += new PaintEventHandler(pictureBox1_Paint);
         } 
@@ -35,6 +47,8 @@ namespace lab4
         {
             polygons.Clear();
             currentPolygon.Clear();
+            comboBoxPolygon.Items.Clear();
+            comboBoxPolygon.Refresh();
             _graphics.Clear(Color.White);
             pictureBox1.Invalidate();
         }
@@ -58,7 +72,9 @@ namespace lab4
         {
             if (currentPolygon.Count > 0)
             {
-                polygons.Add(new List<Point>(currentPolygon)); 
+                polygons.Add(new List<Point>(currentPolygon));
+                comboBoxPolygon.Items.Add($"Polygon {polygons.Count}");
+                comboBoxPolygon.Refresh();
                 currentPolygon.Clear();
                 pictureBox1.Invalidate();
             }
@@ -108,6 +124,239 @@ namespace lab4
                     e.Graphics.DrawPolygon(_pen, currentPolygon.ToArray()); // Рисуем полигон
                 }
             }
+        }
+
+        private void comboBoxAthenian_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxAthenian.SelectedIndex)
+            {
+                case 0:
+                    _mode = Mode.None;
+                    break;
+                case 1:
+                    _mode = Mode.Move_Polygon;
+                    break;
+                case 2:
+                    _mode = Mode.Turn_Around_Point;
+                    break;
+                case 3:
+                    _mode = Mode.Turn_Around_Center;
+                    break;
+                case 4:
+                    _mode = Mode.Scaling_Relative_To_Point;
+                    break;
+                case 5:
+                    _mode = Mode.Scaling_Relative_To_Center;
+                    break;
+            }
+        }
+
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            if (_mode == Mode.None)
+            {
+                //Ваш код
+            }
+            else
+            {
+                switch (_mode)
+                {
+                    case Mode.Move_Polygon:
+                        string[] s = textBoxInput.Text.Split(' ');
+                        int dx = 0, dy = 0;
+                        if (comboBoxPolygon.SelectedIndex == -1 || s.Length > 2 || !int.TryParse(s[0], out dx) || !int.TryParse(s[1], out dy))
+                        {
+                            textBoxOutput.Text = "Ошибка входных данных";
+                            return;
+                        }
+
+                        for (int i = 0; i < polygons[comboBoxPolygon.SelectedIndex - 1].Count; i++)
+                            polygons[comboBoxPolygon.SelectedIndex - 1][i] = Movepoint(polygons[comboBoxPolygon.SelectedIndex - 1][i], dx, dy);
+                        comboBoxAthenian.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        comboBoxPolygon.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        textBoxInput.Text = "";
+                        textBoxOutput.Text = "";
+                        pictureBox1.Invalidate();
+                        break;
+                    case Mode.Turn_Around_Point:
+                        string[] input = textBoxInput.Text.Split(' ');
+                        if (comboBoxPolygon.SelectedIndex == -1 || input.Length != 3)
+                        {
+                            textBoxOutput.Text = "Ошибка входных данных. Введите x, y и угол поворота.";
+                            return;
+                        }
+                        int pointX, pointY, turn;
+                        if (!int.TryParse(input[0], out pointX) || !int.TryParse(input[1], out pointY) || !int.TryParse(input[2], out turn))
+                        {
+                            textBoxOutput.Text = "Ошибка ввода: некорректные данные.";
+                            return;
+                        }
+                        Point rotationPoint = new Point(pointX, pointY);
+                        for (int i = 0; i < polygons[comboBoxPolygon.SelectedIndex - 1].Count; i++)
+                        {
+                            polygons[comboBoxPolygon.SelectedIndex - 1][i] = RotatePoint(polygons[comboBoxPolygon.SelectedIndex - 1][i], rotationPoint, turn);
+                        }
+                        comboBoxAthenian.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        comboBoxPolygon.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        textBoxInput.Text = "";
+                        textBoxOutput.Text = "";
+                        pictureBox1.Invalidate();
+                        break;
+                    case Mode.Turn_Around_Center:
+                        if (comboBoxPolygon.SelectedIndex == -1 || !int.TryParse(textBoxInput.Text, out turn))
+                        {
+                            textBoxOutput.Text = "Ошибка входных данных";
+                            return;
+                        }
+                        Point tempP = PolygonCenter(polygons[comboBoxPolygon.SelectedIndex - 1]);
+                        for (int i = 0; i < polygons[comboBoxPolygon.SelectedIndex - 1].Count; i++)
+                            polygons[comboBoxPolygon.SelectedIndex - 1][i] = RotatePoint(polygons[comboBoxPolygon.SelectedIndex - 1][i], tempP, turn);
+                        comboBoxAthenian.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        comboBoxPolygon.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        textBoxInput.Text = "";
+                        textBoxOutput.Text = "";
+                        pictureBox1.Invalidate();
+                        break;
+                    case Mode.Scaling_Relative_To_Center:
+                        float k = 0f;
+                        if (comboBoxPolygon.SelectedIndex == 0 || !float.TryParse(textBoxInput.Text, out k))
+                        {
+                            textBoxOutput.Text = "Ошибка входных данных";
+                            return;
+                        }
+
+                        Point center = PolygonCenter(polygons[comboBoxPolygon.SelectedIndex - 1]);
+
+                        for (int i = 0; i < polygons[comboBoxPolygon.SelectedIndex - 1].Count; i++)
+                            polygons[comboBoxPolygon.SelectedIndex - 1][i] = ScalePoint(polygons[comboBoxPolygon.SelectedIndex - 1][i], center, k);
+                        comboBoxAthenian.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        comboBoxPolygon.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        textBoxInput.Text = "";
+                        textBoxOutput.Text = "";
+                        pictureBox1.Invalidate();
+                        break;
+                    case Mode.Scaling_Relative_To_Point:
+                        string[] scaleInput = textBoxInput.Text.Split(' ');
+                        if (comboBoxPolygon.SelectedIndex == -1 || scaleInput.Length > 4)
+                        {
+                            textBoxOutput.Text = "Ошибка входных данных. Введите x, y и коэффициент масштабирования.";
+                            return;
+                        }
+
+                        int scaleX, scaleY;
+                        float scaleFactor;
+                        if (!int.TryParse(scaleInput[0], out scaleX) || !int.TryParse(scaleInput[1], out scaleY) || !float.TryParse(scaleInput[2], out scaleFactor))
+                        {
+                            textBoxOutput.Text = "Ошибка ввода: некорректные данные.";
+                            return;
+                        }
+
+                        Point scalePoint = new Point(scaleX, scaleY);
+                        for (int i = 0; i < polygons[comboBoxPolygon.SelectedIndex - 1].Count; i++)
+                        {
+                            polygons[comboBoxPolygon.SelectedIndex - 1][i] = ScalePoint(polygons[comboBoxPolygon.SelectedIndex - 1][i], scalePoint, scaleFactor);
+                        }
+
+                        comboBoxAthenian.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        comboBoxPolygon.SelectedIndex = 0;
+                        comboBoxAthenian.Refresh();
+                        textBoxInput.Text = "";
+                        textBoxOutput.Text = "";
+                        pictureBox1.Invalidate();
+                        break;
+
+
+                }
+            }
+        }
+        private Point Movepoint(Point polygonpoint, int dx, int dy)
+        {
+            int[][] Matrix = new int[3][]
+            {
+                    new int[3] { 1,   0, 0 },
+                    new int[3] { 0,   1, 0 },
+                    new int[3] { dx, dy, 1 }
+            };
+            int[] offsetVector = new int[3] { polygonpoint.X, polygonpoint.Y, 1 };
+            int[] resultVector = Multiplyint(Matrix, offsetVector);
+            return new Point((int)resultVector[0], (int)resultVector[1]);
+        }
+
+         private int[] Multiplyint(int[][] Matrix, int[] array)
+        {
+            int[] resultVector = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    resultVector[i] += Matrix[j][i] * array[j];
+            }
+            return resultVector;
+        }
+        private double[] Multiplydouble(double[][] Matrix, int[] array)
+        {
+            double[] resultVector = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    resultVector[i] += Matrix[j][i] * array[j];
+            }
+            return resultVector;
+        }
+        private Point RotatePoint(Point polygonpoint, Point PointofRotate, int rotateAngle)
+        {
+            double pointA, pointB;
+            double angle = (rotateAngle / 180D) * Math.PI;
+
+            pointA = -PointofRotate.X * Math.Cos(angle) + PointofRotate.Y * Math.Sin(angle) + PointofRotate.X;
+            pointB = -PointofRotate.X * Math.Sin(angle) - PointofRotate.Y * Math.Cos(angle) + PointofRotate.Y;
+
+            int[] offsetVector = new int[3] { polygonpoint.X, polygonpoint.Y, 1 };
+            double[][] Matrix = new double[3][]
+            {
+                new double[3] {  Math.Cos(angle),   Math.Sin(angle), 0 },
+                new double[3] { -Math.Sin(angle),   Math.Cos(angle), 0 },
+                new double[3] { pointA, pointB, 1 }
+            };
+            double[] resultVector = Multiplydouble(Matrix, offsetVector);
+            return new Point((int)resultVector[0], (int)resultVector[1]);
+        }
+        private Point PolygonCenter(List<Point> polygon)
+        {
+            int a = 0;
+            int cx = 0;
+            int cy = 0;
+
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                int temp = polygon[i].X * polygon[(i + 1) % polygon.Count].Y - polygon[(i + 1) % polygon.Count].X * polygon[i].Y;
+                a += temp;
+                cx += (polygon[i].X + polygon[(i + 1) % polygon.Count].X) * temp;
+                cy += (polygon[i].Y + polygon[(i + 1) % polygon.Count].Y) * temp;
+            }
+            a /= 2;
+
+            return new Point(cx / (6 * a), cy / (6 * a));
+        }
+        private Point ScalePoint(Point polygonpoint, Point randompoint, float k)
+        {
+            int[] offsetVector = new int[3] { polygonpoint.X - randompoint.X, polygonpoint.Y - randompoint.Y, 1 };
+            double[][] Matrix = new double[3][]
+            {
+                new double[3] {  k,   0, 0 },
+                new double[3] { 0,   k, 0 },
+                new double[3] { 0, 0, 1 }
+            };
+            double[] resultVector = Multiplydouble(Matrix, offsetVector);
+            return new Point((int)resultVector[0] + randompoint.X, (int)resultVector[1] + randompoint.Y);
         }
     }
 }
