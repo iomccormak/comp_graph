29e53633604917eefe5d9c590b5707b7d87174d8
Stuzja <ann.shishkina04@mail.ru>
2024-10-16 03:34:22 +0300

    feat: added task 2, midpoint displacement

--- lab5/Form3.Designer.cs
+++ lab5/Form3.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.roughnessTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.detailLevelBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailLevelBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 458);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(420, 579);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(165, 23);
+            this.drawButton.TabIndex = 1;
+            this.drawButton.Text = "Построить ломаную";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // roughnessTextBox
+            // 
+            this.roughnessTextBox.Location = new System.Drawing.Point(12, 553);
+            this.roughnessTextBox.Name = "roughnessTextBox";
+            this.roughnessTextBox.Size = new System.Drawing.Size(984, 20);
+            this.roughnessTextBox.TabIndex = 2;
+            this.roughnessTextBox.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 537);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Шероховатость";
+            // 
+            // detailLevelBar
+            // 
+            this.detailLevelBar.Location = new System.Drawing.Point(12, 489);
+            this.detailLevelBar.Maximum = 20;
+            this.detailLevelBar.Name = "detailLevelBar";
+            this.detailLevelBar.Size = new System.Drawing.Size(984, 45);
+            this.detailLevelBar.TabIndex = 5;
+            this.detailLevelBar.Value = 10;
+            this.detailLevelBar.ValueChanged += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 473);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Этапы";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1008, 614);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.detailLevelBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.roughnessTextBox);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form3";
             this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailLevelBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.TextBox roughnessTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar detailLevelBar;
+        private System.Windows.Forms.Label label2;
     }
 }
--- lab5/Form3.cs
+++ lab5/Form3.cs
@@ -1,20 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace lab5
 {
     public partial class Form3 : Form
     {
+        private Bitmap bitmap;
+        private Random random = new Random();
+
         public Form3()
         {
             InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            if (!float.TryParse(roughnessTextBox.Text, out float roughness))
+            {
+                MessageBox.Show("Некорректное значение шероховатости!");
+                return;
+            }
+
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                PointF startPoint = new PointF(0, pictureBox1.Height / 2); 
+                PointF endPoint = new PointF(pictureBox1.Width, pictureBox1.Height / 2); 
+
+                // Recursive call to draw the line
+                MidpointDisplacement(g, startPoint, endPoint, roughness, detailLevelBar.Value);
+              //  FillBeforeBlackLine(bitmap);
+               // DrawStars(g, startPoint, endPoint); 
+            }
+
+            pictureBox1.Image = bitmap; 
+            pictureBox1.Invalidate();
+        }
+
+        private void MidpointDisplacement(Graphics g, PointF start, PointF end, float roughness, int detailLevel)
+        {
+            if (detailLevel <= 0)
+            {
+                g.DrawLine(Pens.Black, start, end);
+            }
+            else
+            {
+                float midX = (start.X + end.X) / 2;
+                float midY = (start.Y + end.Y) / 2;
+                float length = (end.X - start.X) / pictureBox1.Width;
+                float randomOffset = (float)(random.NextDouble() * (roughness * length * 2)) - (roughness * length);
+                midY += randomOffset;
+
+                PointF midPoint = new PointF(midX, midY);
+
+                MidpointDisplacement(g, start, midPoint, roughness, detailLevel - 1);
+                MidpointDisplacement(g, midPoint, end, roughness, detailLevel - 1);
+            }
+        }
+
+        private void DrawStars(Graphics g, PointF start, PointF end)
+        {
+            int starCount = 100; 
+            Brush whiteBrush = new SolidBrush(Color.White);
+            float lineY = (start.Y + end.Y) / 2; 
+            float starHeightOffset = 100; 
+
+            for (int i = 0; i < starCount; i++)
+            {
+                float x = (float)random.NextDouble() * pictureBox1.Width;
+                float y = (float)random.NextDouble() * (lineY - starHeightOffset); 
+                g.FillRectangle(whiteBrush, x, y, 2, 2); 
+            }
+        }
+
+        private void FillBeforeBlackLine(Bitmap img)
+        {
+            Color fillColor = Color.Brown;
+
+            for (int x = 0; x < img.Width; x++)
+            {
+                bool foundBlackPixel = false;
+
+                for (int y = 0; y < img.Height; y++)
+                {
+                    Color pixelColor = img.GetPixel(x, y);
+
+                    if (pixelColor.ToArgb() == Color.Black.ToArgb())
+                    {
+                        foundBlackPixel = true;
+                        break;
+                    }
+
+                    img.SetPixel(x, y, fillColor);
+                }
+
+                if (!foundBlackPixel)
+                {
+                    continue;
+                }
+            }
         }
     }
 }
